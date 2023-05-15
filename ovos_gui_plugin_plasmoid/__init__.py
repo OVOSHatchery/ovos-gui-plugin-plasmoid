ed382e045275b365fae60d2c087ed6a5309c5c9e
JarbasAi <jarbasai@mailfence.com>
2023-05-15 22:51:35 +0100

    :tada: - GUI plugin
    only service containing hardcoded platform specific integration is ovos-gui

---
+++ ovos_gui_plugin_plasmoid/__init__.py
@@ -0,0 +1,20 @@
+from ovos_plugin_manager.templates.gui import GUIExtension
+from ovos_utils.log import LOG
+
+
+class PlasmoidExtension(GUIExtension):
+    """ Plasmoid Platform Extension: This extension is responsible for managing the generic GUI behaviours
+    for non specific platforms. The generic extension does optionally support Homescreen and Homescreen
+    Management but it needs to be exclusively enabled in the configuration file.
+
+    Args:
+        bus: MessageBus instance
+        gui: GUI instance
+        preload_gui (bool): load GUI skills even if gui client not connected
+        permanent (bool): disable unloading of GUI skills on gui client disconnections
+    """
+
+    def __init__(self, bus, gui, config, preload_gui=False, permanent=True):
+        # only thing this is really doing is changing default value of "permanent" flag
+        LOG.info("Plasmoid: Initializing")
+        super().__init__(bus, gui, config, preload_gui, permanent)
